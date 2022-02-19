22d50203d60aa36a287de511bc69c418293ee3b8
wwwilll <98497952+wyaiam@users.noreply.github.com>
2022-02-18 21:38:07 -0500

    setInterval, moveDown + freeze functions

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -62,4 +62,27 @@ document.addEventListener('DOMContentLoaded', () => {
       squares[currentPosition + index].classList.remove('tetromino');
     });
   }
+
+  // make the tetromino move down every second
+  timerId = setInterval(moveDown, 1000);
+
+  // move down function
+  function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+  }
+
+  // freeze function
+  function freeze() {
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+      // start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
+  }
 });
