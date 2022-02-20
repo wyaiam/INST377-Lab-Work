9225da6899590757372a75c50052e427fc3304d6
wwwilll <98497952+wyaiam@users.noreply.github.com>
2022-02-20 18:19:30 -0500

    working button in js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -10,12 +10,29 @@ document.querySelector('#carousel_button--prev').addEventListener("click", funct
   moveToPrevSlide();
 });
 
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
+  }
+
+  slides[slidePosition].classList.add('carousel_item--visible');
+}
+
 function moveToNextSlide() {
   if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
+  } else {
+    slidePosition++;
   }
+  updateSlidePosition();
 }
 
 function moveToPrevSlide() {
-  
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+  updateSlidePosition();
 }
