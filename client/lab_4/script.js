fa7dfc3efe421fbd1bb28ef6c49cd85ba9732d09
wwwilll <98497952+wyaiam@users.noreply.github.com>
2022-02-20 17:53:28 -0500

    change to querySelector

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,19 +1,21 @@
 let slidePosition = 0;
-const slides = document.getElementsByClassName('carousel_item');
+const slides = document.querySelectorAll('.carousel_item');
 const totalSlides = slides.length;
 
-document.getElementById('carousel_button--next').addEventListener("click", function() {
+document.querySelector('#carousel_button--next').addEventListener("click", function() {
   moveToNextSlide();
 });
 
-document.getElementById('carousel_button--prev').addEventListener("click", function() {
+document.querySelector('#carousel_button--prev').addEventListener("click", function() {
   moveToPrevSlide();
 });
 
 function moveToNextSlide() {
-  console.log('hello next')
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  }
 }
 
 function moveToPrevSlide() {
-  console.log('hello prev')
+  
 }
