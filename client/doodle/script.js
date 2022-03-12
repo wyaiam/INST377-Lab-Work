a1b55197ed370bc92af1cda23d000b1965f8ad4a
wwwilll <98497952+wyaiam@users.noreply.github.com>
2022-03-11 19:03:54 -0500

    platform class

--- client/doodle/script.js
+++ client/doodle/script.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const doodler = document.createElement('div');
   let doodlerLeftSpace = 50;
   let doodlerBottomSpace = 150;
+  let isGameOver = false;
+  let platformCount = 5;
+  let platforms = [];
 
   function createDoodler() {
     grid.appendChild(doodler);
@@ -10,5 +13,37 @@ document.addEventListener('DOMContentLoaded', () => {
     doodler.style.left = doodlerLeftSpace + 'px';
     doodler.style.bottom = doodlerBottomSpace + 'px';
   }
-  createDoodler();
+
+  class Platform {
+    constructor(newPlatBottom) {
+      this.bottom = newPlatBottom;
+      this.left = Math.random() * 315;
+      this.visual = document.createElement('div');
+
+      const visual = this.visual;
+      visual.classList.add('platform');
+      visual.style.left = this.left + 'px';
+      visual.style.bottom = this.bottom + 'px';
+      grid.appendChild(visual);
+    }
+  }
+
+  function createPlatforms() {
+    for (let i = 0; i < platformCount; i++) {
+      let platGap = 600 / platformCount;
+      let newPlatBottom = 100 + i * platGap;
+      let newPlatform = new Platform(newPlatBottom);
+      platforms.push(newPlatform);
+      console.log(platforms);
+    }
+  }
+
+  function start() {
+    if (!isGameOver) {
+      createDoodler();
+      createPlatforms();
+    }
+  }
+  // attach to button
+  start();
 });
