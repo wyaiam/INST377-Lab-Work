464ff190f63605ef4e927b35b5fe05580583b13a
wwwilll <98497952+wyaiam@users.noreply.github.com>
2022-03-13 20:04:59 -0400

    copying lab 5 folder

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,14 +1,23 @@
+function dataHandler(dataArray) {
+
+}
+
+// As the last step of your lab, hook this up to index.html
 async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form');
+  const form = document.querySelector('.form'); // change this selector to match the id or classname of your actual form
+  const submit = document.querySelector('.submit_button');
   form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
     submitEvent.preventDefault(); // This prevents your page from refreshing!
     console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
     const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
+    console.table(arrayFromJson); // this is called "dot notation"
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
     // it contains all 1,000 records we need
+    document.addEventListener('DOMContentLoaded', async () => dataHandler());
   });
+
+  // dataHandler();
 }
 
 // this actually runs first! It's calling the function above
