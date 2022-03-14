d504bdcc17e10e527e533419c509be2194efd6a0
wwwilll <98497952+wyaiam@users.noreply.github.com>
2022-03-13 21:30:33 -0400

    final

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,23 +1,59 @@
-function dataHandler(dataArray) {
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
+}
 
+function restoArrayMake(dataArray) {
+  console.log('fired dataHandler');
+  // console.table(dataArray); // this is called "dot notation"
+  const range = [...Array(15).keys()];
+  const listItems = range.map((item, index) => {
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+    return dataArray[restNum];
+  });
+  // console.log(listItems);
+  return listItems;
+  // range.forEach((item) => {
+  //  console.log('range item', item);
+  // });
+}
+
+function createHtmlList(collection) {
+  console.log('fired HTML creator');
+  console.log(collection);
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toUpperCase();
+    const injectThisItem = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
 }
 
 // As the last step of your lab, hook this up to index.html
 async function mainEvent() { // the async keyword means we can make API requests
+  console.log('script loaded');
   const form = document.querySelector('.form'); // change this selector to match the id or classname of your actual form
   const submit = document.querySelector('.submit_button');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
-    document.addEventListener('DOMContentLoaded', async () => dataHandler());
-  });
+  submit.style.display = 'none';
 
-  // dataHandler();
+  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  console.log(arrayFromJson);
+
+  if (arrayFromJson.length > 0) {
+    submit.style.display = 'block';
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a "breakpoint"
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+      const restoArray = restoArrayMake(arrayFromJson);
+      createHtmlList(restoArray);
+    });
+  }
 }
 
 // this actually runs first! It's calling the function above
