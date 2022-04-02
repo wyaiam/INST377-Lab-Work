6464d8c62f1da3de027919b80cdba267a6e9fe9d
wwwilll <98497952+wyaiam@users.noreply.github.com>
2022-04-01 23:11:10 -0400

    border

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
       if (
         obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
-        (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap - 205) ||
+        (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap - 200) ||
         birdBottom === 0) {
         gameOver();
         clearInterval(timerId);
