45fef90909f6034629e75f6af79f0cf0ae96c8b6
wwwilll <98497952+wyaiam@users.noreply.github.com>
2022-04-11 23:12:46 -0400

    fix

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -71,7 +71,7 @@ async function mainEvent() { // the async keyword means we can make API requests
   const retrievalVar = 'restaurants';
   submit.style.display = 'none';
 
-  if (localStorage.getItem(retrievalVar === undefined)) {
+  if (localStorage.getItem(retrievalVar === null)) {
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
     const arrayFromJson = await results.json(); // This changes it into data we can use - an object
     console.log(arrayFromJson);
